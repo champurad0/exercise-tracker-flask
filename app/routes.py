ee098cf41be388598097ee97727842700acbb1f2
champurad0 <ginoduarte17@gmail.com>
2024-07-04 13:21:42 -0700

    added templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/")
 def index():
-    return "chinga su madre."
+    user = {"username": "Gino"}
+    workouts = [
+        {
+            "name": "Squat",
+            "weight": "225",
+            "reps": "5",
+        },
+        {
+            "name": "Pullups",
+            "weight": "0",
+            "reps": "5",
+        },
+    ]
+    return render_template("index.html", title="home", user=user, workouts=workouts)
